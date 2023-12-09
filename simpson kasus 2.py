1e37ea6bb7b32943244e978051a12ea6d4cffc75
EgaPrayoga1 <144421213+EgaPrayoga1@users.noreply.github.com>
2023-12-09 17:00:14 +0700

    Add files via upload

---
+++ simpson kasus 2.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def simpsons_rule(func, a, b, n):
+    h = (b - a) / n
+    x_values = np.linspace(a, b, n+1)
+    integral_sum = func(a) + func(b)
+    
+    for i in range(1, n, 2):
+        integral_sum += 4 * func(x_values[i])
+
+    for i in range(2, n-1, 2):
+        integral_sum += 2 * func(x_values[i])
+
+    return h / 3 * integral_sum
+
+# Fungsi yang diintegralkan
+def integrand(x):
+    return x**2+x+6
+
+# Batas integrasi
+a = 0
+b = 2
+
+# Jumlah subinterval
+n = 4
+
+# Hitung integral menggunakan metode Simpson
+result = simpsons_rule(integrand, a, b, n)
+
+print(f"Hasil integral menggunakan metode Simpson: {result}")
